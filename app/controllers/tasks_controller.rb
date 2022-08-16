2499e7c4697746e66cb03da3607bc8109c8d854a
lucas capparelli <capparelli.lucas@gmail.com>
2022-08-16 19:21:30 -0300

    2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,21 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new # Needed to instantiate the form_for
+  end
+
+  def create
+    @task = Task.new(params[:task])
+    @task.save # Will raise ActiveModel::ForbiddenAttributesError
+
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :complete)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get "tasks/new", to: "tasks#new", as: :new_task
+
   # Defines the root path route ("/")
   # root "articles#index"
   get "tasks", to: "tasks#index"
   get "tasks/:id", to: "tasks#show", as: :task
+
+
+  post "tasks", to: "tasks#create"
 end
